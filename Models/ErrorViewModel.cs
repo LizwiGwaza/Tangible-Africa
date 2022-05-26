a6c0eb1f1da436e13b0e6f19c0f6382e8cd04371
Abongile Boja <s219339279@mandela.ac.za>
2022-05-26 21:45:34 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tangible_Africa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
